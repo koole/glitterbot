8c5ef6687b41a28ac8b7346b7f76024c385be434
Leon Koole <leon@koole.io>
2019-08-18 12:40:21 +0200

    📸 Convert all single frame gifs to pngs

--- images.js
+++ images.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-const util = require("util");
 const md5File = require("md5-file");
+const readimage = require("readimage");
+var Jimp = require('jimp');
 
 // Set this to true if you want to remove all images larger than 2MB
 const removeBig = false;
@@ -34,9 +35,45 @@ for (const folder of folders) {
     if (file.match(/\.((?:gif|jpg|jpeg|png))(?:[\?#]|$)/i)) {
       const path = `${imageFolder}/${folder}`;
       const hash = md5File.sync(`${path}/${file}`);
-      const name = `${hash}.${file.split(".").pop()}`;
-      fs.renameSync(`${path}/${file}`, `${path}/${name}`);
-      images.push(name);
+      const filetype = file.split(".").pop();
+      const name = `${hash}.${filetype}`;
+      
+      // Check if gifs have multiple frames
+      if(filetype == "gif") {
+        var gifFile = fs.readFileSync(`${path}/${file}`)
+        readimage(gifFile, function (err, image) {
+          if (err) {
+            console.log("failed to parse the image")
+            console.log(err)
+          }
+          const frames = image.frames.length;
+          // If a gif has only a single frame it gets converted into a PNG file
+          // so Telegram will show it properly
+          if(frames === 1) {
+            const pngName = `${path}/${hash}.png`;
+            console.log(`${path}/${name} has 1 frame, converting to PNG`)
+            Jimp.read(gifFile, (err, image) => {
+              if (err) throw err;
+              image.write(pngName);
+              images.push(pngName);
+              // Remove the old gif file
+              try {
+                fs.unlinkSync(`${path}/${file}`)
+              } catch(err) {
+                console.error(err)
+              }
+            });
+          // Otherwise also only rename the file
+          } else {
+            fs.renameSync(`${path}/${file}`, `${path}/${name}`);
+            images.push(name);
+          }
+        })
+      // Rename all non gif files right away
+      } else {
+        fs.renameSync(`${path}/${file}`, `${path}/${name}`);
+        images.push(name);
+      }
 
       // Check for filesize, see issue #1
       const { size } = fs.statSync(`${path}/${name}`);
