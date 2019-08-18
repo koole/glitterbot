30e304a96fb5479dfdbf65c943891f1943825ab8
Leon Koole <leon@koole.io>
2019-08-18 12:55:29 +0200

    🚸 Use crc32 instead of md5

--- images.js
+++ images.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const md5File = require("md5-file");
 const readimage = require("readimage");
-var Jimp = require('jimp');
+const Jimp = require("jimp");
+const { crc32 } = require('crc');
 
 // Set this to true if you want to remove all images larger than 2MB
 const removeBig = false;
@@ -34,42 +34,44 @@ for (const folder of folders) {
   for (const file of files) {
     if (file.match(/\.((?:gif|jpg|jpeg|png))(?:[\?#]|$)/i)) {
       const path = `${imageFolder}/${folder}`;
-      const hash = md5File.sync(`${path}/${file}`);
+      const hash = crc32(fs.readFileSync(`${path}/${file}`, "utf8")).toString(
+        16
+      );
       const filetype = file.split(".").pop();
       const name = `${hash}.${filetype}`;
-      
+
       // Check if gifs have multiple frames
-      if(filetype == "gif") {
-        var gifFile = fs.readFileSync(`${path}/${file}`)
-        readimage(gifFile, function (err, image) {
+      if (filetype == "gif") {
+        var gifFile = fs.readFileSync(`${path}/${file}`);
+        readimage(gifFile, function(err, image) {
           if (err) {
-            console.log("failed to parse the image")
-            console.log(err)
+            console.log("failed to parse the image");
+            console.log(err);
           }
           const frames = image.frames.length;
           // If a gif has only a single frame it gets converted into a PNG file
           // so Telegram will show it properly
-          if(frames === 1) {
+          if (frames === 1) {
             const pngName = `${path}/${hash}.png`;
-            console.log(`${path}/${name} has 1 frame, converting to PNG`)
+            console.log(`${path}/${name} has 1 frame, converting to PNG`);
             Jimp.read(gifFile, (err, image) => {
               if (err) throw err;
               image.write(pngName);
               images.push(pngName);
               // Remove the old gif file
               try {
-                fs.unlinkSync(`${path}/${file}`)
-              } catch(err) {
-                console.error(err)
+                fs.unlinkSync(`${path}/${file}`);
+              } catch (err) {
+                console.error(err);
               }
             });
-          // Otherwise also only rename the file
+            // Otherwise also only rename the file
           } else {
             fs.renameSync(`${path}/${file}`, `${path}/${name}`);
             images.push(name);
           }
-        })
-      // Rename all non gif files right away
+        });
+        // Rename all non gif files right away
       } else {
         fs.renameSync(`${path}/${file}`, `${path}/${name}`);
         images.push(name);
